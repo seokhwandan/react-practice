955f52b8c0b2d619f94d6f550aafc8b35b1eabdc
seokhwandan <dansh87@gmail.com>
2022-01-07 15:42:10 +0900

    redux middleware practice

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import rootReducer from './modules'
+// import loggerMiddleware from './lib/loggerMiddleware'
+import { createLogger } from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
+import reportWebVitals from './reportWebVitals'
+
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
